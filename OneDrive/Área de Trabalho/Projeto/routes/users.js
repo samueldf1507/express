3777519ae9b5b2201d543363a15a9a4e285b453c
Samuel De Francesco <defrancescodev@gmail.com>
2024-10-14 16:46:20 -0300

    express

--- OneDrive/Área de Trabalho/Projeto/routes/users.js
+++ OneDrive/Área de Trabalho/Projeto/routes/users.js
@@ -19,8 +19,43 @@ router.get("/", (req, res) => {
     //res.send("User New Form")
 //})
 
+
 router.get("/new", (req, res) => {
     res.send("User New Form")
 })
+
+//Creating a new user
+router.post("/",  (req, res) => {
+    res.send("Create a user")
+})
+
+//id = dynamic parameter
+router.use(logger)
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user)
+    res.send(`Get user with ID ${req.params.id}`)
+   })
+  .post((req, res) => {
+    res.send(`Update user with ID ${req.params.id}`)
+   })
+  .delete((req, res) => {
+    res.send(`Delete user with ID ${req.params.id}`)
+  })
+
+const users = [{ name: "Kyle"}, {name :  "John"}]
+
+router.param("id", (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
+
+function logger(req, res, next) {
+    console.log(req.originalUrl)
+    next()
+}
+
 
 module.exports = router
--- OneDrive/Área de Trabalho/Projeto/server.js
+++ OneDrive/Área de Trabalho/Projeto/server.js
@@ -1,13 +1,15 @@
-//Importing express
+//Importing express 
 const express = require("express")
 
+//Creating a variable to call express functions
 const app = express()
 
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    //Sending data
-
+    //Sending error status
+    //res.status(500).send("Internal Server Error")
+    
     //Sending a file to the user do download
     //res.download("server.js")
 
@@ -22,4 +24,5 @@ const userRouter = require("./routes/users")
 //Linking up the routes to users path
 app.use("/users", userRouter)
 
+//Running the application
 app.listen(3000)
